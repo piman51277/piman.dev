1b90101ce856723f2b7a925384acb10d21fb8b41
piman51277 <piman.dev@gmail.com>
2023-09-28 11:56:36 -0400

    add additional sorting logic

--- src/projects/projects.ts
+++ src/projects/projects.ts
@@ -5,6 +5,23 @@ export let projects: Project[] = [];
 export let isProjectsLoaded = false;
 
 getProjects().then((gotProjects) => {
+  //sort by year, and then by name
+  gotProjects.sort((a, b) => {
+    if (a.year > b.year) {
+      return -1;
+    } else if (a.year < b.year) {
+      return 1;
+    } else {
+      if (a.name > b.name) {
+        return 1;
+      } else if (a.name < b.name) {
+        return -1;
+      } else {
+        return 0;
+      }
+    }
+  });
+
   projects = gotProjects;
   isProjectsLoaded = true;
 });
