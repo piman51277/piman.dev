8f26d3b51c5864efecebba0d2c01be8fa669bb20
piman51277 <piman.dev@gmail.com>
2024-07-09 02:19:59 -0400

    add project loader

---
+++ src/projects/getChildReferences.ts
@@ -0,0 +1,88 @@
+import { existsSync, readFileSync } from "fs";
+
+/**
+ * Whether a file can be parsed by this module
+ * @param {string} path The path to the file
+ * @returns {boolean} Whether the file can be parsed
+ */
+export function canParseFile(path: string): boolean {
+  return path.endsWith(".html") || path.endsWith(".css");
+}
+
+/**
+ * Parses a file and returns a list of all the child references
+ * @param {string} path The path to the file
+ * @returns {string[]} A list of all the child references
+ */
+export function getChildReferences(path: string): string[] {
+  if (!existsSync(path)) {
+    throw new Error(`File does not exist: ${path}`);
+  }
+
+  const contents = readFileSync(path, "utf8");
+
+  if (path.endsWith(".html")) {
+    return getChildReferencesHTML(contents);
+  } else if (path.endsWith(".css")) {
+    return getChildReferencesCSS(contents);
+  }
+
+  //generally we can only handle html and css, so if it's neither, don't bother
+  else {
+    return [];
+  }
+}
+
+import { parse } from "node-html-parser";
+
+const HTMLSearchTags = [
+  "a",
+  "link",
+  "script",
+  "img",
+  "image",
+  "iframe",
+  "object",
+  "embed",
+  "source",
+  "video",
+  "audio",
+  "track",
+];
+
+const HTMLSearchAttributes = ["href", "src", "data", "poster", "srcset"];
+
+/**
+ * Parses an HTML file and returns a list of all the child references
+ * @param {string} html raw html
+ * @returns {string[]} A list of all the child references
+ */
+function getChildReferencesHTML(html: string): string[] {
+  const root = parse(html);
+  const children = [];
+
+  for (const tag of HTMLSearchTags) {
+    const elements = root.querySelectorAll(tag);
+    for (const element of elements) {
+      for (const attr of HTMLSearchAttributes) {
+        const value = element.getAttribute(attr);
+        if (value) {
+          children.push(value);
+        }
+      }
+    }
+  }
+
+  return children;
+}
+
+/**
+ * Parses a CSS file and returns a list of all the child references
+ * @param {string} css raw css
+ * @returns {string[]} A list of all the child references
+ */
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function getChildReferencesCSS(css: string): string[] {
+  //TODO: implement using a proper css parser
+  return [];
+}
---
+++ src/projects/index.ts
@@ -0,0 +1,35 @@
+import { IManifest, loadFileManifest } from "./manifest";
+
+export const projects: IManifest[] = [];
+
+import { existsSync, mkdirSync, readdirSync, rmSync } from "fs";
+import { join } from "path";
+import { loadProject } from "./loader";
+
+//empty out the host directory
+const hostDir = join(__dirname, "../../host");
+if (existsSync(hostDir)) {
+  rmSync(hostDir, { recursive: true });
+}
+mkdirSync(hostDir, { recursive: true });
+
+//create the projects directory, if it doesn't exist
+const projectsDir = join(__dirname, "../../projects");
+if (!existsSync(projectsDir)) {
+  mkdirSync(projectsDir);
+}
+
+//start loading projects
+const projectDirs = readdirSync(projectsDir, { withFileTypes: true });
+for (const projectDir of projectDirs) {
+  if (projectDir.isDirectory()) {
+    const projectPath = join(projectsDir, projectDir.name);
+    const manifestPath = join(projectsDir, projectDir.name, "manifest.json");
+    if (existsSync(manifestPath)) {
+      console.log(`Loading project at ${projectPath}`);
+      const manifest = loadFileManifest(manifestPath);
+      const project = loadProject(manifest, join(projectsDir, projectDir.name));
+      projects.push(project);
+    }
+  }
+}
